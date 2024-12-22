659df39c3ec381503a4e89ec157970db323721d7
aditishankar0630 <118357289+aditishankar0630@users.noreply.github.com>
2024-12-22 23:22:56 +0530

    Update customer_segmentation.py

--- customer_segmentation.py
+++ customer_segmentation.py
@@ -1,1 +1,30 @@
+#Importing the Libraries
+import numpy as np
+import pandas as pd
+import datetime
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt, numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import AgglomerativeClustering
+from matplotlib.colors import ListedColormap
+from sklearn import metrics
+import warnings
+import sys
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+np.random.seed(42)
+
+#Loading the dataset
+data = pd.read_csv("../input/customer-personality-analysis/marketing_campaign.csv", sep="\t")
+print("Number of datapoints:", len(data))
+data.head()
+
 
